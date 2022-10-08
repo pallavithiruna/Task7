d04b7c80acab2ef6e7c306af2fa5ca8afe847efc
pallavithiruna <114101268+pallavithiruna@users.noreply.github.com>
2022-10-08 13:52:19 +0530

    Add files via upload

--- mrf.js
+++ mrf.js
@@ -1,25 +1,25 @@
 //1.a) get all the countries from the asian continent/region using filter function.
-var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v2/all");
-request.send();
-request.onload=function(){
-  var res=JSON.parse(request.response);
-    console.log(res);
-   var result=res.filter((ele)=>ele.region=="Asia").map((ele)=>ele.name);
-    console.log(result);
-    };
-
-
- //1.b) get all the countries whoes population less than 2 lakhs using filter function 
 //var request=new XMLHttpRequest();
 //request.open("GET","https://restcountries.com/v2/all");
 //request.send();
 //request.onload=function(){
-//var res=JSON.parse(request.response);
-//console.log(res);
-//var popu=res.filter((ele)=>ele.population<200000).map((ele)=>ele.name);
-//console.log(popu);
-//};
+  //var res=JSON.parse(request.response);
+   // console.log(res);
+   //var result=res.filter((ele)=>ele.region=="Asia").map((ele)=>ele.name);
+    //console.log(result);
+    //};
+
+
+ //1.b) get all the countries whoes population less than 2 lakhs using filter function 
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+var res=JSON.parse(request.response);
+console.log(res);
+var popu=res.filter((ele)=>ele.population<200000).map((ele)=>ele.name);
+console.log(popu);
+};
 
  //1.c)print the followin details name capital flag using forEach function.
 //var request=new XMLHttpRequest();
